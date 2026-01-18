a3dded0187402f49da2b380746bd5386fdd48784
princeV1411 <princevaishnav323@gmail.com>
2026-01-18 06:32:08 -0800

    phone screen theam change

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 let login =document.querySelector("#sig")
     login.addEventListener("click",function(){
         window.location.href = "other-page/login-page/login.html";
-});
+    }
+);
 
 
 let sem1pdf = document.querySelector(".sem1");
     sem1pdf.addEventListener("click",function(){
         window.location.href = "src/sem1.pdf";
+    }
+);
+
+let result = document.querySelector(".box5")
+    result.addEventListener("click",function(){
+        alert("Site On Construction, Function Call Succesfully");
+    }
+);
+
+let update = document.querySelector(".box6")
+    update.addEventListener("click",function(){
+        alert("Site On Construction. No Updates Now");
+    }
+);
+
+document.querySelector(".box1").addEventListener("click", function(){
+    document.querySelector("#links").scrollIntoView({
+        behavior:"smooth"
+    });
 });
